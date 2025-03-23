85dcafd97b8bfecacac7deab3ea57f10afcd00a6
astos <deuvoid@gmail.com>
2025-03-23 10:48:03 -0300

    Adding new route; searchig for specific words

--- src/main/java/read/bibleSearch/controller/BibleController.java
+++ src/main/java/read/bibleSearch/controller/BibleController.java
@@ -1,12 +1,16 @@
 package read.bibleSearch.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import read.bibleSearch.model.Verses;
 import read.bibleSearch.repository.VerseRepository;
-
+import org.springframework.data.domain.Pageable;
 import java.util.Optional;
+
+
 
 @RestController
 @RequestMapping("/api/bible")
@@ -20,14 +24,32 @@ public class BibleController {
 
     // Search a specific verse
     @GetMapping("/{bookId}/{chapter}/{verse}")
-    public Verses getVerse(@PathVariable int bookId, @PathVariable int chapter, @PathVariable int verse) {
-
+    public Verses getVerse(
+            @PathVariable int bookId,
+            @PathVariable int chapter,
+            @PathVariable int verse) {
         Optional<Verses> result = repository.findByBookIdAndChapterAndVerse(bookId, chapter, verse);
 
         System.out.println("Querying for book: " + bookId + ", chapter: " + chapter + ", verse: " + verse);
         System.out.println("Result found: " + result);
 
         return result.orElseThrow(() -> new RuntimeException("Verse not found"));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<?> searchByKeyWord(
+            @RequestParam String query,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Verses> results = repository.findByTextContainingIgnoreCase(query, pageable);
+
+        if(results.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Verse not found");
+        }
+
+        return ResponseEntity.ok(results);
     }
 }
 
--- src/main/java/read/bibleSearch/repository/VerseRepository.java
+++ src/main/java/read/bibleSearch/repository/VerseRepository.java
@@ -1,10 +1,11 @@
 package read.bibleSearch.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import read.bibleSearch.model.Verses;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,6 @@ public interface VerseRepository extends JpaRepository<Verses, Integer> {
 
     // Search for a specific verse by book, chapter and verse number
     public Optional<Verses> findByBookIdAndChapterAndVerse(int bookId, int chapter, int verse);
+
+    Page<Verses> findByTextContainingIgnoreCase(String query, Pageable pageable);
 }
